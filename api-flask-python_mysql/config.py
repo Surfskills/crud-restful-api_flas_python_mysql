68b1c9a334a171c3ec774ef9982e98feda9a946d
Surfskills <ojjfred@gmail.com>
2023-02-12 02:23:41 -0800

    CRUD

---
+++ api-flask-python_mysql/config.py
@@ -0,0 +1,17 @@
+from app import app
+
+from flaskext.mysql import MySQL
+
+
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mama1111'
+
+app.config['MYSQL_DATABASE_DB'] = 'experiolearn'
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
